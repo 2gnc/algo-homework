e1fa545e9dd5776f34227ec5d92981a51ff9ae77
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-04-06 15:41:07 +0300

    реализован поиск с помощью регулярного выражения

--- src/Suggestr.js
+++ src/Suggestr.js
@@ -1,3 +1,31 @@
+// вариант 1 - сравнение регулярками
+// вариант 2 - приведение к одному регистру и сравнение посимвольно
+// вариант 3 - что-нибудь экзотическое
+
+// *** предпосылки ***
+
 export default class Suggestr {
+  
+  constructor(data) {
+    this.data = data;
+    this.dataVol = data.length;
+  }
+
+  suggest(input) {
+    const variants = [];
+    let count = 0;
+    const mask = new RegExp(input, 'i');
+    for (let i = 0; i <= this.dataVol; i += 1) {
+
+      if (mask.test(this.data[i])) {
+        count += 1;
+        variants.push(this.data[i]);
+      }
+      if (count === 10) {
+        break;
+      }
+    }
+    return variants;
+  }
 
 }
--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import types from './eventTypes';
+import streets from './streets';
 import Event from './Event';
 import Emitter from './Emitter';
 import scrb from './subscriber';
+import Suggestr from './Suggestr';
 
 const evA = new Event(types.EV_ONE, 'evA');
 const em = new Emitter();
 
-const x = scrb(null, 10000);
+const x = scrb(1);
 x.forEach((itm) => {
   em.on(evA, itm);
 });
@@ -14,3 +16,9 @@ const tSt = new Date();
 em.emit(evA);
 console.log('время выполнения', new Date() - tSt);
 
+const Sug = new Suggestr(streets);
+
+const dd = new Date();
+console.log(Sug.suggest('мари'));
+
+console.log(new Date() - dd);
