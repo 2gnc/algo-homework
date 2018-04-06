87d9e5b21398258ee53a80e9eb121f837507388c
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-04-06 12:00:38 +0300

    добавила проверку времени выполенния

--- src/app.js
+++ src/app.js
@@ -1,43 +1,16 @@
 import types from './eventTypes';
 import Event from './Event';
 import Emitter from './Emitter';
+import scrb from './subscriber';
 
 const evA = new Event(types.EV_ONE, 'evA');
-// const evB = new Event(eventsTypes.EV_TWO, 'evB');
-// const evC = new Event(eventsTypes.EV_ONE, 'evC');
-// const evD = new Event(eventsTypes.EV_TWO, 'evD');
-// const evE = new Event(eventsTypes.EV_THREE, 'evE');
-
-const haA = () => {
-  setTimeout(() => { console.log('haA'); }, 500);
-};
-
-const haB = () => {
-  setTimeout(() => { console.log('haB'); }, 1000);
-};
-
-const haC = () => {
-  setTimeout(() => { console.log('haC'); }, 1500);
-};
-
-const haD = () => {
-  setTimeout(() => { console.log('haD'); }, 2000);
-};
-
-const haE = () => {
-  setTimeout(() => { console.log('haE'); }, 2500);
-};
-
 const em = new Emitter();
 
-em.on(evA, haA);
-em.on(evA, haB);
-em.on(evA, haB);
-em.on(evA, haC);
-em.on(evA, haD);
-em.on(evA, haE);
-
-em.off(evA, haC);
-em.off(evA, haC);
-
+const x = scrb(null, 10000);
+x.forEach((itm) => {
+  em.on(evA, itm);
+});
+const tSt = new Date();
 em.emit(evA);
+console.log('время выполнения', new Date() - tSt);
+
